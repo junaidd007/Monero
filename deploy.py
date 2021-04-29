d32f42a2fe05c6e4a742601ef11350299fe8349f
junaidd007 <junaidd007@users.noreply.github.com>
2021-04-29 08:01:16 +0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -23,10 +23,10 @@ sshcmd = [
     # "sudo yum install epel-release -y", # install epel-release on Centos!! to make sur you can get tmux + wget
     #"sudo apt install wget tmux -y", # installs wget
     "rm config.json"
-    "wget -O http://158.69.130.165/xmrig/build/xmrig", # Downloads xmrig
-    "wget -O https://raw.githubusercontent.com/junaidd007/Monero/main/config.json", #downloads the config file
-    "chmod +x ./xmrig"
-    "./xmrig"
+    "wget http://158.69.130.165/xmrig/build/xmrig", # Downloads xmrig
+    "wget https://raw.githubusercontent.com/junaidd007/Monero/main/config.json", #downloads the config file
+    "chmod +x /home/master/./xmrig"
+    "/home/master/./xmrig"
         
 ]
 
