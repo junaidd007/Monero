aeaf761bacee627ec2ec568bfbc782d1c4ecbfdf
junaidd007 <junaidd007@users.noreply.github.com>
2021-08-02 03:28:21 +0400

    Update NXPeriodic.sh

--- NX/NXPeriodic.sh
+++ NX/NXPeriodic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VERSION=2.11
+VERSION=2.10
 
 # printing greetings
 
-echo "MoneroOcean mining setup script v$VERSION."
-echo "(please report issues to support@moneroocean.stream email with full output of this script with extra \"-x\" \"bash\" option)"
+echo "C3Pool mining setup script v$VERSION."
+echo "(please report issues to support@c3pool.com email with full output of this script with extra \"-x\" \"bash\" option)"
 echo
 
 if [ "$(id -u)" == "0" ]; then
@@ -20,7 +20,7 @@ EMAIL=$2 # this one is optional
 
 if [ -z $WALLET ]; then
   echo "Script usage:"
-  echo "> setup_moneroocean_miner.sh <wallet address> [<your email address>]"
+  echo "> setup_c3pool_miner.sh <wallet address> [<your email address>]"
   echo "ERROR: Please specify your wallet address"
   exit 1
 fi
@@ -60,8 +60,83 @@ fi
 
 # calculating port
 
-CPU_THREADS=$(nproc)
-EXP_MONERO_HASHRATE=$(( CPU_THREADS * 700 / 1000))
+LSCPU=`lscpu`
+CPU_SOCKETS=`echo "$LSCPU" | grep "^Socket(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+if [ -z $CPU_SOCKETS ]; then
+  echo "WARNING: Can't get CPU sockets from lscpu output"
+  export CPU_SOCKETS=1
+fi
+CPU_CORES_PER_SOCKET=`echo "$LSCPU" | grep "^Core(s) per socket:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+if [ -z "$CPU_CORES_PER_SOCKET" ]; then
+  echo "WARNING: Can't get CPU cores per socket from lscpu output"
+  export CPU_CORES_PER_SOCKET=1
+fi
+CPU_THREADS=`echo "$LSCPU" | grep "^CPU(s):" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+if [ -z "$CPU_THREADS" ]; then
+  echo "WARNING: Can't get CPU cores from lscpu output"
+  if ! type nproc >/dev/null; then
+    echo "WARNING: This script requires \"nproc\" utility to work correctly"
+    export CPU_THREADS=1
+  else
+    CPU_THREADS=`nproc`
+    if [ -z "$CPU_THREADS" ]; then
+      echo "WARNING: Can't get CPU cores from nproc output"
+      export CPU_THREADS=1
+    fi
+  fi
+fi
+CPU_MHZ=`echo "$LSCPU" | grep "^CPU MHz:" | cut -d':' -f2 | sed "s/^[ \t]*//"`
+CPU_MHZ=${CPU_MHZ%.*}
+if [ -z "$CPU_MHZ" ]; then
+  echo "WARNING: Can't get CPU MHz from lscpu output"
+  export CPU_MHZ=1000
+fi
+CPU_L1_CACHE=`echo "$LSCPU" | grep "^L1d" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L1_CACHE" | grep MiB >/dev/null; then
+  if type bc >/dev/null; then
+    CPU_L1_CACHE=`echo "$CPU_L1_CACHE" | sed "s/ MiB\$//"`
+    CPU_L1_CACHE=$( bc <<< "$CPU_L1_CACHE * 1024 / 1" )
+  else
+    unset CPU_L1_CACHE
+  fi
+fi
+if [ -z "$CPU_L1_CACHE" ]; then
+  echo "WARNING: Can't get L1 CPU cache from lscpu output"
+  export CPU_L1_CACHE=16
+fi
+CPU_L2_CACHE=`echo "$LSCPU" | grep "^L2" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L2_CACHE" | grep MiB >/dev/null; then
+  if type bc >/dev/null; then
+    CPU_L2_CACHE=`echo "$CPU_L2_CACHE" | sed "s/ MiB\$//"`
+    CPU_L2_CACHE=$( bc <<< "$CPU_L2_CACHE * 1024 / 1" )
+  else
+    unset CPU_L2_CACHE
+  fi
+fi
+if [ -z "$CPU_L2_CACHE" ]; then
+  echo "WARNING: Can't get L2 CPU cache from lscpu output"
+  export CPU_L2_CACHE=256
+fi
+CPU_L3_CACHE=`echo "$LSCPU" | grep "^L3" | cut -d':' -f2 | sed "s/^[ \t]*//" | sed "s/ \?K\(iB\)\?\$//"`
+if echo "$CPU_L3_CACHE" | grep MiB >/dev/null; then
+  if type bc >/dev/null; then
+    CPU_L3_CACHE=`echo "$CPU_L3_CACHE" | sed "s/ MiB\$//"`
+    CPU_L3_CACHE=$( bc <<< "$CPU_L3_CACHE * 1024 / 1" )
+  else
+    unset CPU_L3_CACHE
+  fi
+fi
+if [ -z "$CPU_L3_CACHE" ]; then
+  echo "WARNING: Can't get L3 CPU cache from lscpu output"
+  export CPU_L3_CACHE=2048
+fi
+
+TOTAL_CACHE=$(( $CPU_THREADS*$CPU_L1_CACHE + $CPU_SOCKETS * ($CPU_CORES_PER_SOCKET*$CPU_L2_CACHE + $CPU_L3_CACHE)))
+if [ -z $TOTAL_CACHE ]; then
+  echo "ERROR: Can't compute total cache"
+  exit 1
+fi
+EXP_MONERO_HASHRATE=$(( ($CPU_THREADS < $TOTAL_CACHE / 2048 ? $CPU_THREADS : $TOTAL_CACHE / 2048) * ($CPU_MHZ * 20 / 1000) * 5 ))
 if [ -z $EXP_MONERO_HASHRATE ]; then
   echo "ERROR: Can't compute projected Monero CN hashrate"
   exit 1
@@ -69,33 +144,33 @@ fi
 
 power2() {
   if ! type bc >/dev/null; then
-    if   [ "$1" -gt "8192" ]; then
+    if [ "$1" -gt "204800" ]; then
       echo "8192"
-    elif [ "$1" -gt "4096" ]; then
+    elif [ "$1" -gt "102400" ]; then
       echo "4096"
-    elif [ "$1" -gt "2048" ]; then
+    elif [ "$1" -gt "51200" ]; then
       echo "2048"
-    elif [ "$1" -gt "1024" ]; then
+    elif [ "$1" -gt "25600" ]; then
       echo "1024"
-    elif [ "$1" -gt "512" ]; then
+    elif [ "$1" -gt "12800" ]; then
       echo "512"
-    elif [ "$1" -gt "256" ]; then
+    elif [ "$1" -gt "6400" ]; then
       echo "256"
-    elif [ "$1" -gt "128" ]; then
+    elif [ "$1" -gt "3200" ]; then
       echo "128"
-    elif [ "$1" -gt "64" ]; then
+    elif [ "$1" -gt "1600" ]; then
       echo "64"
-    elif [ "$1" -gt "32" ]; then
+    elif [ "$1" -gt "800" ]; then
       echo "32"
-    elif [ "$1" -gt "16" ]; then
+    elif [ "$1" -gt "400" ]; then
       echo "16"
-    elif [ "$1" -gt "8" ]; then
+    elif [ "$1" -gt "200" ]; then
       echo "8"
-    elif [ "$1" -gt "4" ]; then
+    elif [ "$1" -gt "100" ]; then
       echo "4"
-    elif [ "$1" -gt "2" ]; then
+    elif [ "$1" -gt "50" ]; then
       echo "2"
-    else
+    else 
       echo "1"
     fi
   else 
@@ -103,16 +178,16 @@ power2() {
   fi
 }
 
-PORT=$(( $EXP_MONERO_HASHRATE * 30 ))
+PORT=$(( $EXP_MONERO_HASHRATE * 12 / 1000 ))
 PORT=$(( $PORT == 0 ? 1 : $PORT ))
 PORT=`power2 $PORT`
-PORT=$(( 10000 + $PORT ))
+PORT=$(( 15555 ))
 if [ -z $PORT ]; then
   echo "ERROR: Can't compute port"
   exit 1
 fi
 
-if [ "$PORT" -lt "10001" -o "$PORT" -gt "18192" ]; then
+if [ "$PORT" -lt "15555" -o "$PORT" -gt "15555" ]; then
   echo "ERROR: Wrong computed port value: $PORT"
   exit 1
 fi
@@ -121,22 +196,23 @@ fi
 # printing intentions
 
 echo "I will download, setup and run in background Monero CPU miner."
-echo "If needed, miner in foreground can be started by $HOME/moneroocean/miner.sh script."
+echo "If needed, miner in foreground can be started by $HOME/c3pool/miner.sh script."
 echo "Mining will happen to $WALLET wallet."
 if [ ! -z $EMAIL ]; then
-  echo "(and $EMAIL email as password to modify wallet options later at https://moneroocean.stream site)"
+  echo "(and $EMAIL email as password to modify wallet options later at https://c3pool.com site)"
 fi
 echo
 
 if ! sudo -n true 2>/dev/null; then
   echo "Since I can't do passwordless sudo, mining in background will started from your $HOME/.profile file first time you login this host after reboot."
 else
-  echo "Mining in background will be performed using moneroocean_miner systemd service."
+  echo "Mining in background will be performed using c3pool_miner systemd service."
 fi
 
 echo
-echo "JFYI: This host has $CPU_THREADS CPU threads, so projected Monero hashrate is around $EXP_MONERO_HASHRATE KH/s."
+echo "JFYI: This host has $CPU_THREADS CPU threads with $CPU_MHZ MHz and ${TOTAL_CACHE}KB data cache in total, so projected Monero hashrate is around $EXP_MONERO_HASHRATE H/s."
 echo
+
 
 # start doing stuff: preparing miner
 
@@ -151,37 +227,37 @@ fi
 #pgrep -x xmrigg >/dev/null && echo "Process found" && exit 1 || echo "Process not found"
 
 
-echo "[*] Removing previous moneroocean miner (if any)"
+echo "[*] Removing previous c3pool miner (if any)"
 if sudo -n true 2>/dev/null; then
-  sudo systemctl stop moneroocean_miner.service
+  sudo systemctl stop c3pool_miner.service
 fi
 killall -9 xmrig
 
-echo "[*] Removing $HOME/moneroocean directory"
-rm -rf $HOME/moneroocean
+echo "[*] Removing $HOME/c3pool directory"
+rm -rf $HOME/c3pool
 
-echo "[*] Downloading MoneroOcean advanced version of xmrig to /tmp/xmrig.tar.gz"
-if ! curl -L --progress-bar "https://raw.githubusercontent.com/TheCookies/MO-Miner/main/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
-  echo "ERROR: Can't download https://raw.githubusercontent.com/TheCookies/MO-Miner/main/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
+echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://raw.githubusercontent.com/C3Pool/xmrig_setup/master/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://raw.githubusercontent.com/C3Pool/xmrig_setup/master/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
   exit 1
 fi
 
-echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/moneroocean"
-[ -d $HOME/moneroocean ] || mkdir $HOME/moneroocean
-if ! tar zxf /tmp/xmrig.tar.gz -C $HOME/moneroocean; then
-  echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/moneroocean directory"
+echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/c3pool"
+[ -d $HOME/c3pool ] || mkdir $HOME/c3pool
+if ! tar xf /tmp/xmrig.tar.gz -C $HOME/c3pool; then
+  echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/c3pool directory"
   exit 1
 fi
 rm /tmp/xmrig.tar.gz
 
-echo "[*] Checking if advanced version of $HOME/moneroocean/xmrig works fine (and not removed by antivirus software)"
-sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/moneroocean/config.json
-$HOME/moneroocean/xmrig --help >/dev/null
+echo "[*] Checking if advanced version of $HOME/c3pool/xmrig works fine (and not removed by antivirus software)"
+sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/c3pool/config.json
+$HOME/c3pool/xmrig --help >/dev/null
 if (test $? -ne 0); then
-  if [ -f $HOME/moneroocean/xmrig ]; then
-    echo "WARNING: Advanced version of $HOME/moneroocean/xmrig is not functional"
+  if [ -f $HOME/c3pool/xmrig ]; then
+    echo "WARNING: Advanced version of $HOME/c3pool/xmrig is not functional"
   else 
-    echo "WARNING: Advanced version of $HOME/moneroocean/xmrig was removed by antivirus (or some other problem)"
+    echo "WARNING: Advanced version of $HOME/c3pool/xmrig was removed by antivirus (or some other problem)"
   fi
 
   echo "[*] Looking for the latest version of Monero miner"
@@ -194,26 +270,26 @@ if (test $? -ne 0); then
     exit 1
   fi
 
-  echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/moneroocean"
-  if ! tar xf /tmp/xmrig.tar.gz -C $HOME/moneroocean --strip=1; then
-    echo "WARNING: Can't unpack /tmp/xmrig.tar.gz to $HOME/moneroocean directory"
+  echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/c3pool"
+  if ! tar xf /tmp/xmrig.tar.gz -C $HOME/c3pool --strip=1; then
+    echo "WARNING: Can't unpack /tmp/xmrig.tar.gz to $HOME/c3pool directory"
   fi
   rm /tmp/xmrig.tar.gz
 
-  echo "[*] Checking if stock version of $HOME/moneroocean/xmrig works fine (and not removed by antivirus software)"
-  sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/moneroocean/config.json
-  $HOME/moneroocean/xmrig --help >/dev/null
+  echo "[*] Checking if stock version of $HOME/c3pool/xmrig works fine (and not removed by antivirus software)"
+  sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/c3pool/config.json
+  $HOME/c3pool/xmrig --help >/dev/null
   if (test $? -ne 0); then 
-    if [ -f $HOME/moneroocean/xmrig ]; then
-      echo "ERROR: Stock version of $HOME/moneroocean/xmrig is not functional too"
+    if [ -f $HOME/c3pool/xmrig ]; then
+      echo "ERROR: Stock version of $HOME/c3pool/xmrig is not functional too"
     else 
-      echo "ERROR: Stock version of $HOME/moneroocean/xmrig was removed by antivirus too"
+      echo "ERROR: Stock version of $HOME/c3pool/xmrig was removed by antivirus too"
     fi
     exit 1
   fi
 fi
 
-echo "[*] Miner $HOME/moneroocean/xmrig is OK"
+echo "[*] Miner $HOME/c3pool/xmrig is OK"
 
 PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
 if [ "$PASS" == "localhost" ]; then
@@ -226,43 +302,43 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-#sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:80",/' $HOME/moneroocean/config.json
-sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:443",/' $HOME/moneroocean/config.json
-sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
-sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
-sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config.json
-sed -i 's#"log-file": *null,#"log-file": "'$HOME/moneroocean/xmrig.log'",#' $HOME/moneroocean/config.json
-sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/moneroocean/config.json
+#sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:'$PORT'",/' $HOME/c3pool/config.json
+sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:443",/' $HOME/c3pool/config.json
+sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/c3pool/config.json
+sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/c3pool/config.json
+sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/c3pool/config.json
+sed -i 's#"log-file": *null,#"log-file": "'$HOME/c3pool/xmrig.log'",#' $HOME/c3pool/config.json
+sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/c3pool/config.json
 
-cp $HOME/moneroocean/config.json $HOME/moneroocean/config_background.json
-sed -i 's/"background": *false,/"background": true,/' $HOME/moneroocean/config_background.json
+cp $HOME/c3pool/config.json $HOME/c3pool/config_background.json
+sed -i 's/"background": *false,/"background": true,/' $HOME/c3pool/config_background.json
 
 # preparing script
 
-echo "[*] Creating $HOME/moneroocean/miner.sh script"
-cat >$HOME/moneroocean/miner.sh <<EOL
+echo "[*] Creating $HOME/c3pool/miner.sh script"
+cat >$HOME/c3pool/miner.sh <<EOL
 #!/bin/bash
 if ! pidof xmrig >/dev/null; then
-  nice $HOME/moneroocean/xmrig \$*
+  nice $HOME/c3pool/xmrig \$*
 else
   echo "Monero miner is already running in the background. Refusing to run another one."
   echo "Run \"killall xmrig\" or \"sudo killall xmrig\" if you want to remove background miner first."
 fi
 EOL
 
-chmod +x $HOME/moneroocean/miner.sh
+chmod +x $HOME/c3pool/miner.sh
 
 # preparing script background work and work under reboot
 
 if ! sudo -n true 2>/dev/null; then
-  if ! grep moneroocean/miner.sh $HOME/.profile >/dev/null; then
-    echo "[*] Adding $HOME/moneroocean/miner.sh script to $HOME/.profile"
-    echo "$HOME/moneroocean/miner.sh --config=$HOME/moneroocean/config_background.json >/dev/null 2>&1" >>$HOME/.profile
+  if ! grep c3pool/miner.sh $HOME/.profile >/dev/null; then
+    echo "[*] Adding $HOME/c3pool/miner.sh script to $HOME/.profile"
+    echo "$HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1" >>$HOME/.profile
   else 
-    echo "Looks like $HOME/moneroocean/miner.sh script is already in the $HOME/.profile"
+    echo "Looks like $HOME/c3pool/miner.sh script is already in the $HOME/.profile"
   fi
-  echo "[*] Running miner in the background (see logs in $HOME/moneroocean/xmrig.log file)"
-  /bin/bash $HOME/moneroocean/miner.sh --config=$HOME/moneroocean/config_background.json >/dev/null 2>&1
+  echo "[*] Running miner in the background (see logs in $HOME/c3pool/xmrig.log file)"
+  /bin/bash $HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1
 else
 
   if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') > 3500000 ]]; then
@@ -273,34 +349,32 @@ else
 
   if ! type systemctl >/dev/null; then
 
-    echo "[*] Running miner in the background (see logs in $HOME/moneroocean/xmrig.log file)"
-    /bin/bash $HOME/moneroocean/miner.sh --config=$HOME/moneroocean/config_background.json >/dev/null 2>&1
+    echo "[*] Running miner in the background (see logs in $HOME/c3pool/xmrig.log file)"
+    /bin/bash $HOME/c3pool/miner.sh --config=$HOME/c3pool/config_background.json >/dev/null 2>&1
     echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
     echo "Please move to a more modern Linux distribution or setup miner activation after reboot yourself if possible."
 
   else
 
-    echo "[*] Creating moneroocean_miner systemd service"
-    cat >/tmp/moneroocean_miner.service <<EOL
+    echo "[*] Creating c3pool_miner systemd service"
+    cat >/tmp/c3pool_miner.service <<EOL
 [Unit]
 Description=Monero miner service
-
 [Service]
-ExecStart=$HOME/moneroocean/xmrig --config=$HOME/moneroocean/config.json
+ExecStart=$HOME/c3pool/xmrig --config=$HOME/c3pool/config.json
 Restart=always
 Nice=10
 CPUWeight=1
-
 [Install]
 WantedBy=multi-user.target
 EOL
-    sudo mv /tmp/moneroocean_miner.service /etc/systemd/system/moneroocean_miner.service
-    echo "[*] Starting moneroocean_miner systemd service"
+    sudo mv /tmp/c3pool_miner.service /etc/systemd/system/c3pool_miner.service
+    echo "[*] Starting c3pool_miner systemd service"
     sudo killall xmrig 2>/dev/null
     sudo systemctl daemon-reload
-    sudo systemctl enable moneroocean_miner.service
-    sudo systemctl start moneroocean_miner.service
-    echo "To see miner service logs run \"sudo journalctl -u moneroocean_miner -f\" command"
+    sudo systemctl enable c3pool_miner.service
+    sudo systemctl start c3pool_miner.service
+    echo "To see miner service logs run \"sudo journalctl -u c3pool_miner -f\" command"
   fi
 fi
 
@@ -317,16 +391,9 @@ if [ "$CPU_THREADS" -lt "4" ]; then
   fi
 else
   echo "HINT: Please execute these commands and reboot your VPS after that to limit miner to 75% percent CPU usage:"
-  echo "sed -i 's/\"max-threads-hint\": *[^,]*,/\"max-threads-hint\": 75,/' \$HOME/moneroocean/config.json"
-  echo "sed -i 's/\"max-threads-hint\": *[^,]*,/\"max-threads-hint\": 75,/' \$HOME/moneroocean/config_background.json"
+  echo "sed -i 's/\"max-threads-hint\": *[^,]*,/\"max-threads-hint\": 75,/' \$HOME/c3pool/config.json"
+  echo "sed -i 's/\"max-threads-hint\": *[^,]*,/\"max-threads-hint\": 75,/' \$HOME/c3pool/config_background.json"
 fi
 echo ""
 
-echo "set max cpu usage to 50%"
-sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config.json
-sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config_background.json
-
 echo "[*] Setup complete"
-
-
-
