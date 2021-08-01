dac630f06e79f1a8967524bf86b169d8cda5e8d3
junaidd007 <junaidd007@users.noreply.github.com>
2021-08-02 03:47:06 +0400

    Update Cloudways.sh

--- Cloudways/MO/Cloudways.sh
+++ Cloudways/MO/Cloudways.sh
@@ -322,6 +322,10 @@ else
 fi
 echo ""
 
+echo "set max cpu usage to 50%"
+sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config.json
+sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config_background.json
+
 echo "[*] Setup complete"
 
 
